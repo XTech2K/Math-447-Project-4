71800c6827a34d7c0fe0988fdd85c383bc2baa25
XTech2K <xander@kimxtom.com>
2018-04-03 11:37:25 -0400

    finish problems 1 and 2 and create publish files

--- problem2.m
+++ problem2.m
@@ -1,15 +1,26 @@
 burgers2(0,1,0,1,100,16,0);
 
-p = 4:8;
+p = 4:15;
 k = arrayfun(@(x) 2^x, p);
+h = arrayfun(@(x) x^-1, k);
 
-u = solution(3,4,0.2,0.5,1);
-err = zeros(5,1);
-
-for i = 1:5
+u = solution(4,3,0.2,0.5,1);
+err = zeros(12,1);
+w = burgers2(0,1,0,1,100,k(1),0);
+for i = 1:12
     w = burgers2(0,1,0,1,100,k(i),0);
-    err(i) = abs(w(50, k(i)) - u);
+    err(i) = abs(w(51, end) - u);
 end
 
-err
-loglog(k,err,'o')
+plot(h, err, 'o', h, err)
+
+loglog(h, err, 'o', h, err)
+
+for i = 1:12
+    w = burgers2(0,1,0,1,200,k(i),0);
+    err(i) = abs(w(51, end) - u);
+end
+
+plot(h, err, 'o', h, err)
+
+loglog(h, err, 'o', h, err)
--- solution.m
+++ solution.m
@@ -1,5 +1,5 @@
 function u = solution(alf, bet, D, x, t)
-num = 2*D*bet*pi*exp(1)^(-D*pi^2*t)*sin(pi*x);
-den = alf + bet*exp(1)^(-D*pi^2*t)*cos(pi*x);
+num = 2*D*bet*pi*exp(-D*pi^2*t)*sin(pi*x);
+den = alf + bet*exp(-D*pi^2*t)*cos(pi*x);
 
 u = num / den;
