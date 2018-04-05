07a6c714bf38d219d30a6f7c6fa247dd79dcf29a
XTech2K <xander@kimxtom.com>
2018-04-05 10:29:33 -0400

    Add labels and titles to problem 2 graphs

--- problem2.m
+++ problem2.m
@@ -21,14 +21,18 @@ end
 plot(h, err, 'o', h, err)
 xlabel('step size')
 ylabel('error')
+title('Regular Error Plot (100 Space Steps)')
 
 %%
 
 loglog(h, err, 'o', h, err)
 xlabel('log(step size)')
 ylabel('log(error)')
+title('Log-Log Error Plot (100 Space Steps)')
 
 %%
+% As an added experiment, we halved the space interval since it
+% seemed to be the limiting factor on reducing the error.
 
 for i = 1:12
     w = burgers2(0,1,0,1,200,k(i),0);
@@ -37,12 +41,15 @@ end
 
 %%
 
+
 plot(h, err, 'o', h, err)
 xlabel('step size')
 ylabel('error')
+title('Regular Error Plot (200 Space Steps)')
 
 %%
 
 loglog(h, err, 'o', h, err)
 xlabel('log(step size)')
 ylabel('log(error)')
+title('Log-Log Error Plot (200 Space Steps)')
